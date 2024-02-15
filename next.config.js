ee12666b8a9be96c20bd036bc0860a4c808ef350
Alan Gama <dethfiregrasp@gmail.com>
2024-02-14 21:55:17 -0300

    Edge removed

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   productionBrowserSourceMaps: false, // enable browser source map generation during the production build
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  experimental: {
-    appDir: true,
-  },
   // fix all before production. Now it slow the develop speed.
   eslint: {
     // Warning: This allows production builds to successfully complete even if
